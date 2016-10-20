6e7e6ffe9f7ad9dce160d2b1cce7e3903d643d7b
FabFisk <fischettofabrzio@gmail.com>
2016-10-20 09:36:15 +0200

    Fix.

--- JSPconDB/src/it/alfasoft/fabrizio/service/Service.java
+++ JSPconDB/src/it/alfasoft/fabrizio/service/Service.java
@@ -1,5 +1,7 @@
 package it.alfasoft.fabrizio.service;
 
+import java.security.MessageDigest;
+
 import it.alfasoft.fabrizio.bean.UtenteBean;
 import it.alfasoft.fabrizio.dao.UtenteBeanDAO;
 
@@ -36,7 +38,18 @@ public class Service {
 	}	
 	
 	public String codificaPsw(String psw){	
-		return psw;
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] array = md.digest(psw.getBytes());
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < array.length; ++i) {
+				sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100)
+						.substring(1, 3));
+			}
+			return sb.toString();
+		} catch (java.security.NoSuchAlgorithmException e) {
+		}
+		return null;
 	}
 
 }
